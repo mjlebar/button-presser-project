54a0e1ac2494f16079f0a33b9c303df7b8ab6334
mjlebar <106131602+mjlebar@users.noreply.github.com>
2023-04-24 20:57:54 -0500

    Add user based tracking of button press

--- button-app/src/App.js
+++ button-app/src/App.js
@@ -2,14 +2,37 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [presses, setPresses] = useState(0);
+  const [presses, setPresses] = useState({
+    Blue: 0,
+    Green: 0,
+    Red: 0,
+    Orange: 0,
+  });
+  const [user, setUser] = useState("Blue");
+  const handleChange = (e) => {
+    setUser(e.target.value);
+  };
   return (
     <div className="App">
       <header>
         <p>Welcome to the button presser!</p>
       </header>
-      <button onClick={() => setPresses(presses + 1)}>Press me</button>
-      <p>Number of times button pressed: {presses}</p>
+      <button
+        className={user}
+        onClick={() => setPresses({ ...presses, [user]: presses[user] + 1 })}
+      >
+        Press me
+      </button>
+      <p>Number of times button pressed: {presses[user]}</p>
+      <label>
+        Select user:
+        <select value={user} onChange={handleChange}>
+          <option value="Green">Green</option>
+          <option value="Orange">Orange</option>
+          <option value="Red">Red</option>
+          <option value="Blue">Blue</option>
+        </select>
+      </label>
     </div>
   );
 }
